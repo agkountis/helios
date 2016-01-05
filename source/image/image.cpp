89883d9bc37b145e4d648d079f568ea06b22296b
Aggelos Gountis <agelosbb@gmail.com>
2016-01-05 19:01:10 +0200

    Changes in the Image class. Implemented the write_pixel function. Made the tone_map function public and renamed it to tone_map_pixel. The function save_as_ppm no longer tone maps the pixels before saving them as it's not it's job to do.

--- source/image/image.cpp
+++ source/image/image.cpp
@@ -11,14 +11,14 @@ static std::string extract_file_extension(const std::string &file_name)
 
     dot_idx = file_name.find_last_of('.');
 
-    if(dot_idx < 0) {
+    if (dot_idx < 0) {
         std::cerr << "ERROR: Fine name has no extension! "
                 "Could auto-detect the image file format."
                 " Please provide a supported file format extension." << std::endl;
         return "";
     }
 
-    return file_name.substr((unsigned long)dot_idx);
+    return file_name.substr((unsigned long) dot_idx);
 }
 
 /* ------------------------------------------------------------------*/
@@ -29,7 +29,7 @@ bool Image::save_as_ppm(const std::string &file_name)
 {
     std::ofstream file(file_name, std::ios::binary | std::ios::out);
 
-    if(!file.is_open()) {
+    if (!file.is_open()) {
         std::cerr << "Could not open file " << file_name << " for writing!" << std::endl;
         return false;
     }
@@ -46,17 +46,16 @@ bool Image::save_as_ppm(const std::string &file_name)
     /**
      * Write the values to the file.
      */
-    for(int i = 0 ; i < width * height * 3; i += 3) {
+    for (int i = 0; i < width * height * 3; i += 3) {
 
         float *r = &pixels[i];
         float *g = &pixels[i + 1];
         float *b = &pixels[i + 2];
 
-        tone_map(r, g, b);
+        unsigned char int_rgb[3] = {(unsigned char) (*r * 255.0), (unsigned char) (*g * 255.0),
+                                    (unsigned char) (*b * 255.0)};
 
-        int int_rgb[3] = { (int)(*r * 255.0), (int)(*g * 255.0), (int)(*b * 255.0) };
-
-        file.write((const char*)&int_rgb[0], 3 * sizeof(int));
+        file.write((const char *) &int_rgb[0], 3 * sizeof(unsigned char));
     }
 
     file.close();
@@ -69,19 +68,12 @@ bool Image::save_auto_detect(const std::string &file_name)
 {
     std::string extension = extract_file_extension(file_name);
 
-    if(extension.compare(".ppm") == 0) {
+    if (extension.compare(".ppm") == 0) {
         return save_as_ppm(file_name);
     }
 
     std::cerr << "ERROR: Unrecognized file format! Please provide a supported file format extension." << std::endl;
     return false;
-}
-
-void Image::tone_map(float *r, float *g, float *b)
-{
-    *r = *r / *r + 1.0f;
-    *g = *g / *g + 1.0f;
-    *b = *b / *b + 1.0f;
 }
 
 /* ---------------------------------------------------------------------- */
@@ -97,9 +89,26 @@ unsigned int Image::get_height() const
 }
 
 
+void Image::write_pixel(float r, float g, float b)
+{
+    pixels = new float(r);
+    pixels++;
+    pixels = new float(g);
+    pixels++;
+    pixels = new float(b);
+    pixels++;
+}
+
+void Image::tone_map_pixel(float *r, float *g, float *b)
+{
+    *r = *r / (*r + 1.0f);
+    *g = *g / (*g + 1.0f);
+    *b = *b / (*b + 1.0f);
+}
+
 bool Image::save(const std::string &file_name, ImageFormat image_format)
 {
-    if(file_name.size() == 0) {
+    if (file_name.size() == 0) {
         std::cerr << "ERROR: Image file name cannot be an empty string! Please provide a valid file name." << std::endl;
         return false;
     }
--- source/image/image.h
+++ source/image/image.h
@@ -15,8 +15,6 @@ private:
 
     bool save_auto_detect(const std::string &file_name);
 
-    void tone_map(float *r, float *g, float *b);
-
 public:
     Image(unsigned int width, unsigned int height) : width(width), height(height)
     { }
@@ -29,6 +27,10 @@ public:
     unsigned int get_width() const;
 
     unsigned int get_height() const;
+
+    void write_pixel(float r, float g, float b);
+
+    void tone_map_pixel(float *r, float *g, float *b);
 
     bool save(const std::string &file_name, ImageFormat image_format = IMG_FMT_AUTO_DETECT);
 };
